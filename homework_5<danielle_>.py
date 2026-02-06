ae1941fd79a91ff7dbb3171ad12141aa5bad8057
danitech-design <dmcle22@gmail.com>
2026-02-06 23:17:40 +0000

    Add comment

---
+++ homework_5<danielle_>.py
@@ -0,0 +1,20 @@
+# beach = ["sunscreen", "towel","sunglasses","swimsuit","flipflops","hat"]
+# print(beach)
+# print("the first two items in the list are:","sunscreen","towel")
+# print("the first two items in the list are:"[0:2])
+# print("the middle two items in the list are:","sunglasses","swimsuit")
+# print("the middle two items in the list are:"[2:3])
+# print("the first and last items in the list are:","sunscreen","hat")
+# print("the first and last items in the list are:"[0:5])
+menu = ("pizza","garlic bread","salad","shrimp scampi","tiramisu")
+print(menu)
+for menufood in menu:
+    print(" happy hour "+menufood)
+
+print("Finished")
+updatedmenu = ("lasagna","mozzarella sticks","shrimp scampi","tiramisu")
+print("updatedmenu")
+for menufood in updatedmenu:
+    print("happy hour " +menufood)
+
+print("Finished")
